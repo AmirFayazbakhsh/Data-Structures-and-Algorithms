40c322b01fa7f101bd08b17adad61a3257a0ae81
amirFayaz <amirfayazfanavar@gmail.com>
2022-02-25 12:38:09 +0330

    recurtion example

--- recursion/Recursive.php
+++ recursion/Recursive.php
@@ -21,22 +21,68 @@ ini_set('display_startup_errors', TRUE);
 			}
 
 			// Recursive Case
+			echo $number.'<br>';
 
 			return ($number * self::factorial($number - 1));
 
 
 
+
+
+			// $data =1;
+
+			// for ($i=1; $i < 5; $i++) { 
+			// 	echo $data."<br>";
+				
+			// 	$data = $data * ($i+1);
+			// }
+
+			// return $data;
+
+
+
+		}
+	}
+		// $obj = new Recursive;
+	// echo  $obj->factorial(5);
+
+
+
+
+	class factClass{
+
+		public function fact($n){
+
+			return ($n >= 1) ? $n * self::fact($n-1)   :  1;
+		}
+
+	}
+
+
+	// $fact = new factClass();
+	// echo $fact->fact(4);
+
+
+
+
+
+
+	class fibonacci{
+
+		public static function fib($n){ 
+
+			return $n >= 3 ? self::fib($n - 1) + self::fib($n - 2) :  1;
 		}
 	}
 
 
-	$obj = new Recursive;
-	echo  $obj->factorial(5);
 
 
 
 
+	$fib = new fibonacci;
 
+	print_r($fib->fib(3));
 
 
 
